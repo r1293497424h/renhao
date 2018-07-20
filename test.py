854a63a167c9b0a9423cb178e9dacea89bf27729
renhao <1293497424@qq.com>
2018-07-20 08:57:38 +0800

    wrote a py flie

---
+++ test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+#创建一个常量op
+m1 = tf.constant([[3,3]])
+#创建一个常量op
+m2 = tf.constant([[2],[3]])
+#创建一个矩阵乘法op，把m1和m2传入
+product = tf.matmul(m1,m2)
+print(product)
+#定义一个会话，启动默认的图
+sess = tf.Session()
+#调用sess的run方法来执行矩阵乘法op
+#run（product）触发了图中的3个op
+result = sess.run(product)
+print(result)
+sess.close()
+
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
